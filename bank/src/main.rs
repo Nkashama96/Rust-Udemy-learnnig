e25df0949e5b284047c9e7c4c5522ca573752f11
Nkashama96 <mockup.gamer6@gmail.com>
2026-01-07 15:52:06 +0700

    Commit 2 on 07/01/2026

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,3 +1,5 @@
+use std::string;
+
 #[derive(Debug)]
 struct Account {
      id: u32,
@@ -10,6 +12,35 @@ struct Bank {
     accounts: Vec<Account>,
 }
 
+impl Bank{
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
+}
+
+impl Account {
+    fn new(id:u32, holder:String) -> Self {
+        Account { id, balance: 0, holder }
+    }
+}
+
+fn print_account(mut account:Account) -> Account{
+    println!("{:#?}",account);
+    account.id =  account.id +1;
+    account
+}
 fn main() {
-    println!("Hello, world!");
+    // let bank = Bank::new();
+
+    // let other_bank = bank;
+
+    let mut account = Account::new(1, String::from("me name"));
+    let other_account = account;
+
+    // println!("{:#?}",other_account);
+    account = print_account(other_account);
+    // account = print_account(other_account);
+    // println!("{:#?}",account);
+    print_account(account);
+
 }
